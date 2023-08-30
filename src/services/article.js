aa4c065e22a2520183ef5218d3fc2911873ef0e3
Aaron Thomas <at2222@ic.ac.uk>
2023-08-30 13:53:38 +0100

    working summariser

--- src/services/article.js
+++ src/services/article.js
@@ -1,38 +1,63 @@
-//This imports the necessary functions from the Redux Toolkit
-// library for creating an API client and handling queries.
+// //This imports the necessary functions from the Redux Toolkit
+// // library for creating an API client and handling queries.
 
 
-import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
+// import {createApi, fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 
-// Fetch the RapidAPI key from environment variables
+// // Fetch the RapidAPI key from environment variables
+
+// const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
+
+
+// // Create an API client using createApi
+// export const articleApi = createApi({
+//     // Name of the slice for the generated Redux reducer
+//     reducerPath: 'articleApi',
+//     baseQuery: fetchBaseQuery({
+//         //which API to call
+//         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
+//         prepareHeaders: (headers) => {
+//             headers.set('X-RapidAPI-Key', rapidApiKey);
+//             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+//             return headers;
+//         }
+
+//     }),
+
+//     endpoints: (builder) => ({
+//          getSummary: builder.query({
+//               query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
+//           })
+//      })
+              
+   
+// });
+
+
+// export const {useLazyGetSummaryQuery} = articleApi;
+
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 
 const rapidApiKey = import.meta.env.VITE_RAPID_API_ARTICLE_KEY;
 
-
-// Create an API client using createApi
 export const articleApi = createApi({
-    // Name of the slice for the generated Redux reducer
     reducerPath: 'articleApi',
     baseQuery: fetchBaseQuery({
-        //which API to call
         baseUrl: 'https://article-extractor-and-summarizer.p.rapidapi.com/',
         prepareHeaders: (headers) => {
             headers.set('X-RapidAPI-Key', rapidApiKey);
             headers.set('X-RapidAPI-Host', 'article-extractor-and-summarizer.p.rapidapi.com');
+
             return headers;
-        }
-
+        },
     }),
-
     endpoints: (builder) => ({
-         getSummary: builder.query({
-              query: (params) => `/summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`
-          })
-     })
-              
-   
-});
+        getSummary: builder.query({
+            // encodeURIComponent() function encodes special characters that may be present in the parameter values
+            // If we do not properly encode these characters, they can be misinterpreted by the server and cause errors or unexpected behavior. Thus that RTK bug
+            query: (params) => `summarize?url=${encodeURIComponent(params.articleUrl)}&length=3`,
+        }),
+    }),
+})
 
-
-export const {useLazyGetSummaryQuery} = articleApi;
-
+export const { useLazyGetSummaryQuery } = articleApi
